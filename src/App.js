7179705bd5fffa210ff08808cff5969e696db01a
Павел Голуб <probel.holub@gmail.com>
2017-12-25 18:23:24 +0300

    begin

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Creator from './Creator';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Creator />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/MyList.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import Creator from './Creator';
+
+class MyList extends Component {
+	constructor(props){
+		super(props);
+		
+	}
+}
